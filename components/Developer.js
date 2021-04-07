251de0ca5d48e6a0d678a69a6d3a23f5dd630d6e
Christopher Song <chris4728.song2001@gmail.com>
2021-04-07 09:29:38 -0500

    Wrap text if needed, refactor styles

--- components/Developer.js
+++ components/Developer.js
@@ -1,29 +1,42 @@
 import React from 'react'
-import { StyleSheet, Image, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, 
+        Image, 
+        Text, 
+        TouchableOpacity, 
+        View, 
+        Dimensions } from 'react-native'
 
 // TODO: avatar/profile picture is currently hardcoded
-const Developer = ({ developer, view }) => (
-  <TouchableOpacity
+const Developer = ({ developer, view }) => {
+  // TODO: maybe scale the font size based on the screen width/height?
+  const scaleFont = () => {
+    const width = Dimensions.get('window').width 
+    return (width > 500 ? 16 : 12)
+  }
+
+  return (
+    <TouchableOpacity
     style={styles.developerButton}
     onPress={() => {
       view(developer)
     }}
   >
     <Image
-      style={{width: 75, height: 75, marginRight: "1%"}}
+      style={styles.developerImage}
       source={{
         uri:
           'https://www.routeone.com/sites/default/files/default_images/default-person.png',
       }}
     />
-    <View style={{flexDirection: 'column'}}>
+    <View style={styles.developerInfo}>
       <Text>{developer.name}</Text>
       <Text>{developer.location}</Text>
       <Text>{developer.role}</Text>
       <Text>{developer.skills.join(', ')}</Text>
     </View>
   </TouchableOpacity>
-)
+  )
+}
 
 const styles = StyleSheet.create({
   developerButton: {
@@ -36,6 +49,15 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
   },
+  developerImage: {
+    width: 75,
+    height: 75,
+    marginRight: 10
+  },
+  developerInfo : {
+    flexDirection: 'column',
+    flexShrink: 1
+  }
 })
 
 export default Developer
