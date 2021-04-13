3dc9ef0591a37c506679fe35957239d89e553751
Christopher Song <chris4728.song2001@gmail.com>
2021-04-12 20:17:49 -0500

    Add sample data (nonprofits)

---
+++ utils/nonProfData.js
@@ -0,0 +1,34 @@
+export const nonprofs = {
+    nonprofits: [
+        {
+            title: 'Web Development for PI180 Website',
+            role: 'Web Developer',
+            company: 'Project Impact 180',
+            descrip: 'We need help building our website',
+            industry: 'Community Leadership',
+            length: 2,
+            weeklyTime: 1,
+            languages: null
+        },
+        {
+            title: 'Update Ongoing Classes',
+            role: 'Website Maintenance',
+            company: 'We All Code',
+            descrip: 'Help us maintain our weekly classes by integrating the information from our schedule into the website each week',
+            industry: 'Education',
+            length: 2,
+            weeklyTime: 2,
+            languages: ['JS', 'CSS', 'HTML']
+        },
+        {
+            title: 'Scholarship/Career Search',
+            role: 'Project Developer',
+            company: 'Evanston Church',
+            descrip: 'Help us build a platform to match our students with scholarships to apply for',
+            industry: 'Education, Church Group',
+            length: 0,
+            weeklyTime: 0,
+            languages: null
+        }
+    ]
+};
