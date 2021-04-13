28bee2cc8a4ca48abd8ec5e5e50d77d67ea469be
Christopher Song <chris4728.song2001@gmail.com>
2021-04-12 20:52:09 -0500

    Yet even more sample data

--- utils/nonProfData.js
+++ utils/nonProfData.js
@@ -16,8 +16,7 @@ export const nonprofs = {
             company: 'We All Code',
             descrip: 'Help us maintain our weekly classes by integrating the information from our schedule into the website each week',
             industry: 'Education',
-            length: 2,
-            languages: ['JS', 'CSS', 'HTML']
+            length: 2
         },
         {
             id: 2,
@@ -67,10 +66,41 @@ export const jobs = {
         {
             companyId: 1,
             title: 'Android Developer',
-            descrip: `Seeking an Android developer `,
+            descrip: `Seeking an Android developer proficient in Kotlin. Industry and/or relevant project
+                    experience preferred.`,
             prereqs: null,
             weeklyTime: 1, // 0: <5 hrs/week, 1: 5-10 hrs, 2: 10+ hrs
-            languages: ['C', 'C++', 'SQL']
+            languages: ['Java', 'XML']
+        },
+        {
+            companyId: 1,
+            title: 'iOS Developer',
+            descrip: `Searching for an iOS developer with significant experience in Swift and/or Objective-C.
+                    Please reach out with a portfolio of prior iOS (or mobile) projects.`,
+            prereqs: null,
+            weeklyTime: 1, // 0: <5 hrs/week, 1: 5-10 hrs, 2: 10+ hrs
+            languages: ['Swift', 'Objective-C']
+        },
+        {
+            companyId: 2,
+            title: 'Web Designer',
+            descrip: `We are looking for somebody to build a website to match students with 
+                    appropriate scholarship. Tech-stack is currently undecided, but we are thinking
+                    React or Angular on the frontend & Node.js on the backend. Added bonus if currently pursuing
+                    a bachelor's or advanced degree in CS.`,
+            prereqs: null,
+            weeklyTime: 2, // 0: <5 hrs/week, 1: 5-10 hrs, 2: 10+ hrs
+            languages: ['HTML', 'JS', 'CSS']
+        },
+        {
+            companyId: 2,
+            title: 'Mobile Developer',
+            descrip: `Searching for a developer proficient in React Native (goal is to deploy
+                    to both iOS and Android efficiently). Ideal candidate will be familiar with React/React Native,
+                    TypeScript, JavaScript, React Hooks, and Redux.`,
+            prereqs: null,
+            weeklyTime: 2, // 0: <5 hrs/week, 1: 5-10 hrs, 2: 10+ hrs
+            languages: ['HTML', 'JS', 'CSS', 'TypeScript']
         }
     ]
 };
