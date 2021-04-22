36c4aa0f8f69a7cb69649d01004f19dc7b55f646
Tiger Nie <nhl0819@gmail.com>
2021-04-22 17:53:44 +0800

    added firebase package and config

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQtBaMx1kJUc3cUhV2OlB_xBTeBG0CIdE',
+  authDomain: 'nonprofdev.firebaseapp.com',
+  databaseURL: 'https://nonprofdev-default-rtdb.firebaseio.com',
+  projectId: 'nonprofdev',
+  storageBucket: 'nonprofdev.appspot.com',
+  messagingSenderId: '135728428832',
+  appId: '1:135728428832:web:868a5bf1ecaab0db0ec945',
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export { firebase }
